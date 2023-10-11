86170c7cfe4f301745cac0012b450146c8ebdccc
Igor.Cardoso <igorfilipicardoso@gmail.com>
2023-10-11 15:18:19 -0300

    Att no javascript

--- scripts/insertMovies.js
+++ scripts/insertMovies.js
@@ -1,29 +1,26 @@
 let template = document.getElementById('template')
-let tempCopy = template;
 let idCount = 0
+
 const popularButton = document.querySelector('#popularity-movies')
 const topRatedButton = document.querySelector('#top-rated-movies')
 const upcomingButton = document.querySelector('#upcoming-movies')
 
 popularButton.addEventListener('click', () => {
-  idCount = 0;
-  document.querySelector('#cards').innerHTML = ''
+  reset()
   request('https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=')
   document.querySelector('#main-title').textContent = 'Filmes populares'
 })
 
 topRatedButton.addEventListener('click', () => {
-  idCount = 0;
-  document.querySelector('#cards').innerHTML = ''
+  reset()
   request('https://api.themoviedb.org/3/movie/top_rated?language=pt-BR&page=')
   document.querySelector('#main-title').textContent = 'Filmes mais bem avaliados'
 })
 
 upcomingButton.addEventListener('click', () => {
-  idCount = 0;
-  document.querySelector('#cards').innerHTML = ''
-  request('https://api.themoviedb.org/3/movie/now_playing?language=pt-BR&page=')
-  document.querySelector('#main-title').textContent = 'Filmes Atuais'
+  reset()
+  request('https://api.themoviedb.org/3/trending/all/week?language=pt-BR')
+  document.querySelector('#main-title').textContent = 'Muitas coisas'
 })
 
 async function request(url = 'https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=') {
@@ -47,28 +44,22 @@ async function request(url = 'https://api.themoviedb.org/3/movie/popular?languag
     } else {
       resultFinal.forEach(movie => {
 
-        template.querySelector('.movie-title').textContent = movie.title
+        if (movie.title == null) {
+          title(movie.name)
+        } else {
+          title(movie.title)
+        }
+
         template.querySelector('.movie-img').setAttribute('src', 'https://image.tmdb.org/t/p/original' + movie.poster_path)
-        template.querySelector('.movie-img').setAttribute('alt', movie.title + ' poster.')
         template.querySelector('.movie-backdrop').setAttribute('src', 'https://image.tmdb.org/t/p/original' + movie.backdrop_path)
-        template.querySelector('.movie-img').setAttribute('alt', movie.title + ' imagem.')
         template.querySelector('.vote-avg').textContent = 'Nota média: ' + movie.vote_average
         template.querySelector('.vote-count').textContent = 'Quantidade de avaliações: ' + movie.vote_count
         template.querySelector('.popularity').textContent = movie.popularity
         template.querySelector('.release-date').textContent = movie.release_date
         template.querySelector('.movie-overview').textContent = movie.overview
 
-        template.querySelector('.modal-title').removeAttribute('id')
-        template.querySelector('.modal-title').setAttribute('id', 'modalTitleId' + idCount)
-        template.querySelector('.modal-title').textContent = movie.title
-
-        template.querySelector('.modal').removeAttribute('id')
-        template.querySelector('.modal').setAttribute('id', 'modal-id-' + idCount)
-
-        template.querySelector('.card-type').removeAttribute('data-bs-target')
-        template.querySelector('.card-type').setAttribute('data-bs-target', '#modal-id-' + idCount)
-
-        template.querySelector('.modal-title').textContent = movie.title
+        removeAtributesOfTemplate();
+        addAtributesOfTemplate();
 
         document.querySelector('#cards').innerHTML += template.innerHTML
 
@@ -78,8 +69,32 @@ async function request(url = 'https://api.themoviedb.org/3/movie/popular?languag
   }
 }
 
+function reset(){
+  idCount = 0;
+  document.querySelector('#cards').innerHTML = ''
+}
+
+function title(movieTitle) {
+  template.querySelector('.movie-title').textContent = movieTitle
+  template.querySelector('.movie-img').setAttribute('alt', movieTitle + ' poster.')
+  template.querySelector('.movie-img').setAttribute('alt', movieTitle + ' imagem.')
+  template.querySelector('.modal-title').textContent = movieTitle
+}
+
 request()
 document.getElementById('template').remove()
+
+function addAtributesOfTemplate() {
+  template.querySelector('.modal-title').setAttribute('id', 'modalTitleId' + idCount)
+  template.querySelector('.modal').setAttribute('id', 'modal-id-' + idCount)
+  template.querySelector('.card-type').setAttribute('data-bs-target', '#modal-id-' + idCount)
+}
+
+function removeAtributesOfTemplate() {
+  template.querySelector('.modal-title').removeAttribute('id')
+  template.querySelector('.modal').removeAttribute('id')
+  template.querySelector('.card-type').removeAttribute('data-bs-target')
+}
 
 function errorComplete(index) {
   template.querySelector('.movie-title').textContent = 'Titulo mt foda';
